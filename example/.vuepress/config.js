e5f3c59dd7fae4202cc23c238c20808bdf322ee5
Pete Davis <oldmanplanet@gmail.com>
2020-06-03 14:48:11 +0100

    Initial layout and npm start Hello World!

---
+++ example/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'VuePress Theme Base | Example Website',
+  description: 'This is an example website built with vuepress-theme-base',
+  theme: require.resolve('../../layouts/Layout.vue'),
+}
