22528c6ecfdb2d4dc1b272811715a206c9f820c0
Pete Davis <oldmanplanet@gmail.com>
2020-06-15 22:37:51 +0100

    Deploy settings

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'Example Website',
   description: 'An example website built with vuepress-theme-base',
+  base: '/vuepress-theme-base/',
   theme: require.resolve('../../theme'),
   themeConfig: {
     nav: [
